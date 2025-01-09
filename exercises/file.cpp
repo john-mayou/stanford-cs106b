638da9daddd26899ba3fc48d8cf4a5a16ed99873
John Mayou <john@johnmayou.com>
2025-01-09 17:32:47 -0600

    exercise 4

--- exercises/file.cpp
+++ exercises/file.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
 int main() {
-  int total = 0;
-  for (int i = 1; i <= 100; i++) {
-    total += i;
+  int n;
+  std::cout << "Enter a number: ";
+  std::cin >> n;
+  int sum = 0;
+  int current = 0;
+  while (n > 0) {
+    if (current % 2 == 1) {
+      sum += current;
+      n -= 1;
+    }
+    if (n == 0) {
+      break;
+    }
+    current += 1;
   }
-  std::cout << "Total: " << total << std::endl;
+  std::cout << "Total: " << sum << std::endl;
   return 0;
 }
