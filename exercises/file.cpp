57ea168bd04171fcef17ccc2bfabf4b13f2373bb
John Mayou <john@johnmayou.com>
2025-01-09 17:44:28 -0600

    exercise 5

--- exercises/file.cpp
+++ exercises/file.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <limits>
+
+const int SENTINEL = 0;
 
 int main() {
-  int n;
-  std::cout << "Enter a number: ";
-  std::cin >> n;
-  int sum = 0;
-  int current = 0;
-  while (n > 0) {
-    if (current % 2 == 1) {
-      sum += current;
-      n -= 1;
+  int max = std::numeric_limits<int>::min();
+  int maxSet = false;
+  std::cout << "Enter " << SENTINEL << " to signal the end of the list."
+            << std::endl;
+  while (true) {
+    int value;
+    std::cout << " ? ";
+    std::cin >> value;
+    if (value == SENTINEL) break;
+    if (!maxSet || value > max) {
+      max = value;
+      maxSet = true;
     }
-    if (n == 0) {
-      break;
-    }
-    current += 1;
   }
-  std::cout << "Total: " << sum << std::endl;
+  if (maxSet) {
+    std::cout << "Maximum number provided was " << max << std::endl;
+  } else {
+    std::cout << "No numbers were provided." << std::endl;
+  }
   return 0;
 }
