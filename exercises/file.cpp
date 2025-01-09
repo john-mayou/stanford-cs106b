2303c694b43d33fbf09beccfe5d6ccb836f32c03
John Mayou <john@johnmayou.com>
2025-01-09 17:28:14 -0600

    exercise 3

--- exercises/file.cpp
+++ exercises/file.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 
 int main() {
-  int m;
-  std::cout << "Enter meters: ";
-  std::cin >> m;
-  float totalInches = m / 0.0254;
-  std::cout << "totalInches: " << totalInches << std::endl;
-  int feet = std::floor(totalInches / 12);
-  float inches = totalInches - (feet * 12);
-  std::cout << "Feet: " << feet << "; Inches: " << inches << std::endl;
-  std::cout << "Feet: " << feet + (inches / 12.0) << std::endl;
+  int total = 0;
+  for (int i = 1; i <= 100; i++) {
+    total += i;
+  }
+  std::cout << "Total: " << total << std::endl;
   return 0;
 }
