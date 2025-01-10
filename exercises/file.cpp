12813a226f851385fe8d14bd0e0048f4c239df3b
John Mayou <john@johnmayou.com>
2025-01-09 18:30:26 -0600

    exercise 8

--- exercises/file.cpp
+++ exercises/file.cpp
@@ -1,21 +1,21 @@
+#include <cmath>
 #include <iostream>
-#include <limits>
-
-const int SENTINEL = -1;
 
 int main() {
-  int scoreTotal = 0;
-  int scoreCount = 0;
-  std::cout << "This program calculates the average of test scores."
-            << std::endl;
-  while (true) {
-    int score;
-    std::cout << "Enter a score: ";
-    std::cin >> score;
-    if (score == SENTINEL) break;
-    scoreTotal += score;
-    scoreCount++;
+  int n;
+  std::cout << "This program reverses the digits in an integer." << std::endl;
+  std::cout << "Enter a positive integer: ";
+  std::cin >> n;
+
+  int r = 0;
+  int degree =
+      int(std::log10(n));  // find maximum degree (9423 -> 10^3.97 -> 3)
+  while (n > 0) {
+    r += (n % 10) * std::pow(10, degree);
+    n /= 10;
+    degree--;
   }
-  std::cout << "Average score: " << scoreTotal / float(scoreCount) << std::endl;
+
+  std::cout << "Reversed: " << r << std::endl;
   return 0;
 }
